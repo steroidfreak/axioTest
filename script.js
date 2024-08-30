bab4c80fa06b9fb95fbc99d2306da8604a6b623b
steroidfreak <134042732+steroidfreak@users.noreply.github.com>
2024-08-30 13:25:47 +0800

    updated with cors-anywhere for testing:

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const API_URL = 'https://wvf6q7huv9.execute-api.ap-southeast-1.amazonaws.com/';
 const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
-// const response = await axios.post(PROXY_URL + API_URL, 
 
 async function sendMessageToChatGPT(message) {
   try {
     console.log('Sending message:', message);  // Log the message being sent
-    const response = await axios.post(API_URL, 
+const response = await axios.post(PROXY_URL + API_URL, 
       { input: message },
       {
         headers: {
